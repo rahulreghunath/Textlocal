7f519219ac55f1a99bc5c9fbe31c57cf8991b000
AKIAJXLBB53ILNKPCX4A <rahulreghunath11@gmail.com>
2018-01-10 01:04:26 +0530

    Default Sender

--- src/Textlocal.php
+++ src/Textlocal.php
@@ -2,32 +2,33 @@
 
 namespace Rahulreghunath\Textlocal;
 
-
 class Textlocal
 {
-    private $key;
+    private $key, $sender;
 
     public function __construct()
     {
 
-        $this->key = config('textlocal.key'); # authentication key
+        $this->key = config('textlocal.key'); # Authentication key
+        $this->sender = config('textlocal.sender'); # Default sender name
 
     }
 
-    public function send($message,$sender)
+    public function send($message, $receiver, $sender = null)
     {
+
         $xmlData = '
-    <SMS>
-    <Account apiKey="'.$this->key.'" Test="0" Info="1" JSON="0">
-    <Sender From="OPTKET">
-    <Messages>
-    <Msg ID="16" Number="' . $sender . '">
-    <Text>' . $message . '</Text>
-    </Msg>
-    </Messages>
-    </Sender>
-    </Account>
-    </SMS>';
+            <SMS>
+            <Account apiKey="' . $this->key . '" Test="0" Info="1" JSON="0">
+            <Sender From="' . $sender != null ? $sender : $this->sender . '">
+            <Messages>
+            <Msg ID="16" Number="' . $receiver . '">
+            <Text>' . $message . '</Text>
+            </Msg>
+            </Messages>
+            </Sender>
+            </Account>
+            </SMS>';
         $post = 'data=' . urlencode($xmlData);
         $url = "http://api.textlocal.in/xmlapi.php";
         $ch = curl_init();
